01ed78d62d47e2a99a74a0587f0d421d10657666
Steve Rodrigue <stev.rodr@gmail.com>
2023-03-14 13:38:27 -0400

    Added the sign application method

--- applications/sdk.go
+++ applications/sdk.go
@@ -1,6 +1,15 @@
 package applications
 
-import identities "github.com/steve-care-software/identities/domain"
+import (
+	identities "github.com/steve-care-software/identities/domain"
+	"github.com/steve-care-software/libs/cryptography/hash"
+)
+
+// OnSignFn represents an onSign fn
+type OnSignFn func(hash hash.Hash, pk []byte) ([]byte, error)
+
+// OnVerifySignatureFn represents an onVerifySignature fn
+type OnVerifySignatureFn func(pubKey []byte, signature []byte, hash hash.Hash) (bool, error)
 
 // Application represents the chain application
 type Application interface {
@@ -9,4 +18,5 @@ type Application interface {
 	Update(name string, updated identities.Identity, originalPassword []byte, newPassword []byte) error
 	Retrieve(name string, password []byte) (identities.Identity, error)
 	Delete(name string, password []byte) error
+	Sign(hash hash.Hash, identity identities.Identity) ([]byte, error)
 }
